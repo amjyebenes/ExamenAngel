4f82050dcd204b98e6304da1a133af7d6457afd2
amjyebenes <amjyebenes@gmail.com>
2022-04-27 09:12:29 +0200

    primer commit con la clase suma

---
+++ src/prueba1/suma.java
@@ -0,0 +1,17 @@
+package prueba1;
+
+import java.util.Scanner;
+
+public class suma {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce el primer numero: ");
+		int n1 = sc.nextInt();
+		System.out.println("Introduce el segundo numero: ");
+		int n2 = sc.nextInt();
+		System.out.println("La suma es: " + (n1+n2));
+
+	}
+
+}
